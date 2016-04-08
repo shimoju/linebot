eaee8d7d33fba34c4a848c78baf3a834c51f75a7
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2016-04-08 16:01:23 +0900

    Implement echo bot

--- app.rb
+++ app.rb
@@ -16,4 +16,30 @@ class App < Sinatra::Base
   get '/' do
     slim :index
   end
+
+  post '/linebot/callback' do
+    params = JSON.parse(request.body.read)
+
+    params['result'].each do |msg|
+      request_content = {
+        to: [msg['content']['from']],
+        toChannel: 1383378250, # Fixed value
+        eventType: '138311608800106203', # Fixed value
+        content: msg['content']
+      }
+
+      endpoint_uri = 'https://trialbot-api.line.me/v1/events'
+      content_json = request_content.to_json
+
+      RestClient.proxy = ENV['FIXIE_URL']
+      RestClient.post(endpoint_uri, content_json, {
+        'Content-Type' => 'application/json; charset=UTF-8',
+        'X-Line-ChannelID' => ENV['LINE_CHANNEL_ID'],
+        'X-Line-ChannelSecret' => ENV['LINE_CHANNEL_SECRET'],
+        'X-Line-Trusted-User-With-ACL' => ENV['LINE_CHANNEL_MID'],
+      })
+    end
+
+    'OK'
+  end
 end
